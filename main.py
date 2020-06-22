f53b123784d07782f33a50153aa61efc17ecacf1
LucasAubrun <lucasaubrun@orange.fr>
2020-06-22 18:47:02 -0400

    Ajout du répertoire de sortir et modification de noms

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import yaml
 from src.services.trainer import Trainer
 
-config_path = 'configs/config.yaml'
+config_path = 'src/configs/configFile.yaml'
+output_dir = 'src/trained'
 
 with open(config_path) as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-trainer = Trainer(config)
+trainer = Trainer(config,output_dir)
 trainer.train()
