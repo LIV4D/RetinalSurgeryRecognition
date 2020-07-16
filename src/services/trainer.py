6d8a6a11fc79cdf1f71c3e93ff4e623b50c1e704
LucasAubrun <lucasaubrun@orange.fr>
2020-07-15 21:23:26 -0400

    Calcul de la courbe ROC et de l'aire (AUC) et sauvegarde sur tensorboard

--- src/services/trainer.py
+++ src/services/trainer.py
@@ -3,7 +3,9 @@ import torch
 import torch.nn as nn
 import os
 import yaml
-from math import log2
+import numpy as np
+
+import sklearn.metrics
 from src.services.abstract_manager import Manager
 
 
@@ -26,6 +28,7 @@ class Trainer(Manager):
         :param e: Current epoch
         :return:
         """
+#DEJA FAIT        #self.writer = SummaryWriter(os.path.join(self.exp_path, 'tensorboard\\'), filename_suffix = "Accuracy")
         dataloader = self.datasetManager.get_dataloader()
         length_dataloader = len(dataloader)
         print("Epoch %i"%e)
@@ -50,7 +53,7 @@ class Trainer(Manager):
                     if valid_loss < self.best_valid_loss:
                         self.best_valid_loss = valid_loss
                         filename = 'trained_model_iter_%i_loss_%.4f.pth'%(index, valid_loss)
-                        filename = os.path.join(self.output_dir, filename)
+                        filename = os.path.join(self.output_dir, 'trained_model', filename)
                         self.network.save_model(filename, optimizers=self.opt)
 
             self.backward_and_step(loss) #On appel la backpropagation
@@ -72,19 +75,61 @@ class Trainer(Manager):
         classification metric
         :param current_index: Current training index=index_current_epoch*nb_batch_per_epoch+index_current_batch
         :return: The average validation loss (scalar)
-        """
-        nnet_out = []
+        """    
+        loss_out = []
+        n_class = self.config['CNN']['n_classes']
+        gts_cat = torch.LongTensor()
+        pred_cat = torch.LongTensor()
+        out_cat = torch.FloatTensor()
         Validation = self.datasetManager.get_validation_dataloader()
         length = len(Validation)
         for i, batch in enumerate(Validation):
             index = current_index*length+i
             batch = self.to_device(batch)
+#            print('%i'%i) pour le suivi
             img = batch[0]
             gts = batch[1]
             out = self.network(img)
+            out = self.softmax(out)
             loss = self.loss(out,gts)
-            return loss
+            pred = torch.argmax(out, 1, keepdim = True)
+            pred = pred.view(-1)
+            loss_out.append(loss.item())
+            
+            gts_cat = torch.cat((gts_cat,gts.cpu()),0)
+            pred_cat = torch.cat((pred_cat,pred.cpu()),0)
+            out_cat = torch.cat((out_cat,out.cpu()),0)
+            
+            
+        gts_cat = gts_cat.numpy()
+        pred_cat = pred_cat.numpy()
+
+        gts_onehot = self.one_hot(gts_cat, n_class)
+        pred_onehot = self.one_hot(pred_cat, n_class)
+        proba = out_cat.numpy()
+        
+        fpr = dict()
+        tpr = dict()
+        AUC_roc = dict()
+        Mean_roc = []
+        for k in range(n_class):
+            fpr[k], tpr[k], _ = sklearn.metrics.roc_curve(gts_onehot[:,k], proba[:,k])
+            AUC_roc[k] = sklearn.metrics.auc(fpr[k], tpr[k])        
+            self.tb_writer.add_scalar('AUC_Roc classe %i'%k, AUC_roc[k], current_index)
+            #self.tb_writer.add_scalars('Roc classe %i'%k, fpr[k], tpr[k])
+            #pas trouvé comment écrire tout un tableau, faire une boucle ? lent, à voir si intéressant
+            
+            Mean_roc.append(AUC_roc[k])
+            
+        Accuracy = sklearn.metrics.accuracy_score(gts_cat,pred_cat)  
+        
+        self.tb_writer.add_scalar('Mean AUC_roc', np.mean(Mean_roc), current_index)
+        self.tb_writer.add_scalar('Accuracy', Accuracy, current_index)
+        self.tb_writer.add_scalar('Loss', np.mean(loss_out), current_index)
+        
+        return np.mean(loss_out)
 #            pass # todo A compléter au besoin
+            #sklearn
 
 
     def setup_optims(self):
@@ -117,3 +162,20 @@ class Trainer(Manager):
         self.opt.zero_grad()
         loss.backward()
         self.opt.step()
+        
+    def one_hot(self, gts, n_class):
+        L = len(gts)        
+        tab = np.zeros((L,n_class))
+        for i in range(L):
+            tab[i,gts[i]] = 1
+        
+        return(tab)
+    
+    def proba_form(self, proba, n_class):
+        L = len(proba)        
+        tab = np.zeros((L,n_class))
+        for i in range(L):
+            tab[i, :] = proba[i]
+        
+        return(tab)        
+
