e410dbdda8dd5b88fbd8b0e135e2ab1e9b9c96c2
LucasAubrun <lucasaubrun@orange.fr>
2020-07-24 01:42:07 -0400

    Selection du ResNet

--- main_test.py
+++ main_test.py
@@ -6,7 +6,7 @@ config_path = 'src/configs/configFile.yaml'
 with open(config_path) as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-tester = Tester(config, '3')
+tester = Tester(config, '1')
 tester.inference()
 
 
