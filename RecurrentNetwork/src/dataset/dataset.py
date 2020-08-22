287e051ee3958dc3e893906a50d2ebb413d0c3db
LucasAubrun <lucasaubrun@orange.fr>
2020-08-22 02:40:30 -0400

    Bug

--- RecurrentNetwork/src/dataset/dataset.py
+++ RecurrentNetwork/src/dataset/dataset.py
@@ -99,7 +99,7 @@ class ImagesDataset(Dataset):
         dtype = array.dtype
         pad = self.RNN_len - shape[0]
         padding = [(0, pad)] + [(0, 0) for _ in shape[1:]]
-        print('Padding\n', padding)
+        print(shape, self.RNN_len, padding)
         padded_array = np.pad(array.detach(), padding, mode='constant', constant_values=-1)
 
         if dtype==int:
