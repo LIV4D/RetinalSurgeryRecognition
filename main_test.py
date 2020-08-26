28111174421baa302f4e55419ad36b9d3f94c704
LucasAubrun <lucasaubrun@orange.fr>
2020-08-26 10:02:50 -0400

    bug

--- main_test.py
+++ main_test.py
@@ -1,7 +1,7 @@
 import yaml
 from src2.services.tester import Tester
 
-config_path = 'src/configs/configFile.yaml'
+config_path = 'src2/configs/configFile.yaml'
 
 with open(config_path) as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
